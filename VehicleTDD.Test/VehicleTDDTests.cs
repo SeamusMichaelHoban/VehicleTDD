bad191923545c74f1d64ef3e01b75493fa3a0ef6
SeamusMichaelHoban <seamushoban2000@gmail.com>
2018-06-06 15:23:13 -0400

    first commit

---
+++ VehicleTDD.Test/VehicleTDDTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleTDD.Test
+{
+    [TestFixture]
+    public class VehicleTDDTest
+    {
+        [Test]
+        public void VehicleTDD_Ctor()
+        {
+            var vehicle = new Vehicle(" "); //Arrange
+
+            var result = vehicle.GetName; //Act
+
+            Assert.That(result, Is.EqualTo(" ")); //Assert
+        }
+        //[Test]
+        //public void VehicleTDD_DefaultCtor()
+        //{
+        //    var vehicle = new Vehicle("DefaultCtor"); //Arrange
+
+        //    var result = vehicle.GetName; //Act
+
+        //    Assert.That(result, Is.EqualTo("DefaultCtor")); //Assert
+        //}
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
