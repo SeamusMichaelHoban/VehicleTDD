597968284ac76978f80c3f7fcdbbb04e10c244fc
SeamusMichaelHoban <seamushoban2000@gmail.com>
2018-08-23 17:41:15 -0400

    fix connection to github

--- VehicleTDD.Test/VehicleTDDTests.cs
+++ VehicleTDD.Test/VehicleTDDTests.cs
@@ -9,26 +9,102 @@ namespace VehicleTDD.Test
 {
     [TestFixture]
     public class VehicleTDDTest
-    {
+    {//Seamus Hoban
         [Test]
-        public void VehicleTDD_Ctor()
+        public void Create_Veh_CTR_1_Input()
         {
-            var vehicle = new Vehicle(" "); //Arrange
+            var vehicle = new Vehicle(); //Arrange
 
-            var result = vehicle.GetName; //Act
+            var result = vehicle.GetName = " "; //Act
 
             Assert.That(result, Is.EqualTo(" ")); //Assert
         }
         //[Test]
-        //public void VehicleTDD_DefaultCtor()
+        //public void Create_Veh_CTR_2_Input()
         //{
-        //    var vehicle = new Vehicle("DefaultCtor"); //Arrange
+        //    var vehicle = new Vehicle("Make", "Model"); //Arrange
 
         //    var result = vehicle.GetName; //Act
 
-        //    Assert.That(result, Is.EqualTo("DefaultCtor")); //Assert
+        //    Assert.That(result, Is.EqualTo("GetName")); //Assert
         //}
+        [Test]
+        public void Create_Veh_CTR_3_Make_Model_Year()
+        {
+            var vehicle = new Vehicle();//Arrange
 
+            var result = vehicle.GetName = "GetName";//Act
+
+            Assert.That(result, Is.EqualTo("GetName"));//Assert
+        }
+        [Test]
+        public void Set_Engine_Size()
+        {
+            var vehicle = new Vehicle(); //Arrange
+
+            var result = vehicle.EngineSize = 5.0; //Act
+
+            Assert.That(result, Is.EqualTo(5.0));//Assert
+        }
+        [Test]
+        public void Set_Rim_Size()
+        {
+            var vehicle = new Vehicle(); //Arrange
+
+            var result = vehicle.RimSize = 16; //Act
+
+            Assert.That(result, Is.EqualTo(16));//Assert
+
+        }
+        [Test]
+        public void Set_Veh_Color()
+        {
+            var vehicle = new Vehicle(); //Arrange
+
+            var result = vehicle.VehColor = "blue"; //Act
+
+            Assert.That(result, Is.EqualTo("blue"));//Assert
+
+        }
+        [TestCase(0, 0, 0)]
+        [TestCase(1, 1, 1)]
+        [TestCase(1, 2, .5)]
+        public void GetMPH_Return_0_For_0(double distance, double time, double expected)
+        {
+            var vehicle = new Vehicle(); //Arrange
+
+            var result = vehicle.GetMPH(distance, time); //Act
+
+            Assert.That(result, Is.EqualTo(expected));//Assert
+        }
+     
+        [Test]
+        public void Create_Get_MPH_From_Timespan()
+        {
+            var vehicle = new Vehicle(); //Arrange
+            var distance = 1;
+            TimeSpan time = TimeSpan.FromMinutes(2);
+
+            var result = vehicle.GetMPH(distance, time); //Act
+
+            Assert.That(result, Is.EqualTo(30));//Assert
+        }
+        [Test]
+        public void Create_Get_MPH_Timespan()
+        {
+            var vehicle = new Vehicle(); //Arrange
+            var distance = 1;
+            TimeSpan time = TimeSpan.FromHours(2);
+
+            var result = vehicle.GetMPH(distance, time); //Act
+
+            Assert.That(result, Is.EqualTo(.5));//Assert
+        }
+        [Test]
+        public void Create_Get_MPG()
+        {
+
+        }
 
 
 
--- VehicleTDD/Vehicle.cs
+++ VehicleTDD/Vehicle.cs
@@ -8,16 +8,40 @@ namespace VehicleTDD
 {
     public class Vehicle
     {
-        
-       
+
         public string GetName { get; set; }
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public string Year { get; set; }
+        public double EngineSize { get; set; }
+        public int RimSize { get; set; }
+        public string VehColor { get; set; }
+
         public Vehicle()
         {
 
         }
-        public Vehicle(string name)
-        { 
-            this.GetName = name;
+        public Vehicle(string model, string make, string year)
+        {
+            this.Model = model;
+            this.Make = make;
+            this.Year = year;
         }
+        
+        public double GetMPH(double distance, double time)
+        {
+            if (time == 0)
+            {
+                return 0;
+            }
+            return distance / time;
+        } 
+
+        public double GetMPH(double distance, TimeSpan time)
+        {
+            double hours = time.TotalHours;
+            return GetMPH(distance, hours);
+        }
+       
     }
 }
